bc676ae6b7e17505d8b44978ec9efd0cbf6849dd
y.zz <me@y.zz>
2014-12-23 15:27:57 +0800

    压力测试请求和lambda回调
    生成压力测试需要的请求参数和
    拿到响应数据后，用来对数据作进一步处理的lambda回调函数

--- php/Log.php
+++ php/Log.php
@@ -3,19 +3,19 @@
  * @author company Sevenga
  * @author Zhen Yang <yz0437@gmail.com>
  * @copyright 2014
- * @Additional instructions :refine Module Function
+ * @Additional instructions :Log
  */
 class Log
 {
 	private static $obj;
-    public $logFile;
     private $logsFile;
-    private $logPath='./';
+    private $logPath='';
 
 	function __construct()
 	{
 		date_default_timezone_set('PRC');
 	}
+    
 	static function getInstance()
 	{
 		if(!self::$obj)
@@ -24,45 +24,20 @@ class Log
 		}
 		return self::$obj;
 	}
-	function  write2($data,$file='applicatiion')
-	{
-        if(!isset($this->logsFile[$file]))
+
+	function  write2($data,$file='applicatiion',$path='./')
+	{  
+        $this->logPath = $path;
+        if(!isset($this->logsFile[$this->logPath.$file]))
 		{
-            $this->logsFile[$file]= fopen($this->logPath.$file.'.log', 'a+');
+            $this->logsFile[$this->logPath.$file]= fopen($this->logPath.$file.'.rb', 'w+');
 		}
-        fwrite($this->logsFile[$file],'['.date('Y-m-d H:i:s').'] '.$data."\n");
+        fwrite($this->logsFile[$this->logPath.$file],$data."\n");
         return $this;
 	}
-    static function  write($data,$file='applicatiion')
+
+    static function  write($data,$file='applicatiion',$path='./')
     {
-        return self::getInstance()->write2($data,$file);
-        // return $this;
-    }
-    function travel_arr($arr,&$result='',$symbolL='')
-    {
-        $symbolLTemp = $symbolL;
-        $symbolL .= '|';
-        foreach ($arr as $key => $value) {
-            if(is_array($value))
-            {
-                if(!is_numeric($key))
-                {      
-                    $result .= $symbolL.$key."\n";
-                    $this->travel_arr($value,$result,$symbolL.'  ');
-                }
-                else
-                {
-                    $result .= $symbolLTemp."[\n";
-                    $this->travel_arr($value,$result,$symbolLTemp . '  ');
-                    $result .=  $symbolLTemp."]\n";
-                    break;
-                }
-            }
-            else
-            {
-                $result .= $symbolL.$key."\n";
-            }
-        }
-        return $result;
+        return self::getInstance()->write2($data,$file,$path);
     }
 }
--- php/getclassfunction.php
+++ php/getclassfunction.php
@@ -5,161 +5,62 @@
  * @copyright 2014
  * @Additional instructions  提取module act作为生成测试脚本
  */
-/**
- * 活动
- * @author 
- *
- */
-class ModActivityCollection {
-	
-	/**
-	 * 获取数据
-	 * @param unknown_type $uid
-	 * @param unknown_type $json
-	 * @param unknown_type $user
-	 */
-	public function get_data($uid, $json, $user) {
-		$ret = array();
-		list($switch, $config_id, $open_at, $close_at) = ActivityCollection::get_activity_setting();
-		if ($switch) {
-			$coll = ActivityCollection::selectActivityCollection($config_id);
-			$player_coll = ActivityPlayerCollection::selectByUid($uid);
-			
-			$ret['config_id'] = $config_id;
-			$ret['open_at'] = $open_at;
-			$ret['close_at'] = $close_at;
-			$ret['all_collect'] = $coll->sum_devote;
-			$ret['stage'] = $coll->stage;
-			$ret['my_collect'] = $player_coll->devote_value;
-		}
-		Output::ok(array(1, $ret));
-	}
-	
-	/**
-	 * 物资贡献
-	 * @param unknown_type $uid
-	 * @param unknown_type $json
-	 * @param unknown_type $user
-	 */
-	public function contribute($uid, $json, $user) {
-		$item_id = $json['item_id'];
-		$item_num = $json['item_num'];
-		if (empty($item_id) || empty($item_num)) {
-			Output::ok(array(0, Lang::$ERROR_PARAMETERS));
-		}
-		list($switch, $config_id, $open_at, $close_at) = ActivityCollection::get_activity_setting();
-		if (!$switch) {
-			Output::ok(array(0, Lang::$ACTIVITY_MESSAGE1));
-		}
-		$item_config = getJsonConfig()->getConfig('collection')->get_item_config($config_id);
-		if (empty($item_config)) {
-			logs("activity_error===");
-			error_log("activity_error===");
-			Output::ok(array(0, Lang::$ACTIVITY_MESSAGE2));
-		}
-		
-		if (empty($item_config[$item_id])) {
-			Output::ok(array(0, Lang::$ACTIVITY_MESSAGE3));
-		}
-		
-		$devote_config = $item_config[$item_id]['devote'];
-		$sum_devote = $devote_config * $item_num;
-		
-		Table::START_TRANSACTION();
-		$coll = ActivityCollection::selectActivityCollection($config_id, true);
-		$player_coll = ActivityPlayerCollection::selectByUid($uid);
-		if ($player_coll->current_stage != $coll->stage) {
-			$player_coll->current_stage = $coll->stage;
-		}
-		
-		$box = array();
-		$result = Item::useItem($uid, $item_id, $item_num);
-		Item::addItem($uid, $item_config[$item_id]['chest'], $item_num, "activity_collection");
-		array_push($box, array($item_config[$item_id]['chest'], $item_num));
-		if (!$result) {
-			Table::ROLLBACK();
-			Output::ok(array(0, Lang::$ITEM_NUM_NOT_ENOUGH));
-		}
-		$coll->sum_devote += $sum_devote;
-		$player_coll->devote_value[$coll->stage - 1] += $sum_devote;
-		$player_coll->sum_devote_value += $sum_devote;
-		
-		list($phase, $standard, $award_list, $sum_stage) = getJsonConfig()->getConfig('collection')->get_stage_config($config_id, $coll->stage);
-		if (empty($phase)) {
-			Table::ROLLBACK();
-			Output::ok(array(0, Lang::$ERROR_CONFIG));
-		}
-		#检测是否达到发放奖励的条件
-		if ($player_coll->devote_value[$coll->stage - 1] >= $standard) {
-			if ($player_coll->award_stage < $coll->stage) {
-				$player_coll->award_stage = $coll->stage;
-				$mcontent = "恭喜您获得奖励";
-				Message::addMessage($uid, MessageType::$COMMON, Lang::$EMAIL_ACTIVITY_MESSAGE2, 0, 0, $award_list);
-			}
-		}
-		
-		#达成了一个阶段
-		if ($coll->sum_devote >= $phase) {
-			$coll->sum_devote = 0;
-			$coll->stage += 1;
-			$player_coll->current_stage = $coll->stage;
-				
-			#所有的目标达成，发放排行榜奖励
-			if ($coll->stage > $sum_stage) {
-				logs("send_award_rank=============>>>");
-				ActivityPlayerCollection::send_award_rank();
-			}
-		}
-		
-		$player_coll->update();
-		$coll->update();
-		Table::COMMIT();
-		
-		Output::ok(array(1, array('box'=>$box)));
-	}
-	
-	/**
-	 * 获取到排行榜
-	 * @param unknown_type $uid
-	 * @param unknown_type $json
-	 */
-	public function get_rank($uid, $json) {
-		$rank = array();
-		$sql = "select a.name, b.sum_devote_value from users as a, activity_player_collection as b where a.uid = b.uid and b.sum_devote_value > 0 order by b.sum_devote_value desc limit 50";
-		$data = Table::selectBySql($sql);
-		foreach ($data as $item) {
-			array_push($rank, array('name'=>$item['name'], 'devote_value'=>$item['sum_devote_value']));
-		}
-		Output::ok(array(1, $rank));
-	}
-}
 
 
-// $nn = new ModActivityCollection();
-// print_r( get_declared_classes());
-print_r(get_class_methods(ModActivityCollection));
+require 'Log.php';
+require 'modules/includes.php';
+// print_r(get_class_methods('ModActivityCollection'));
 
 $a = get_declared_classes();
-
 foreach($a as $v)
 {
 	$t = new ReflectionClass($v);
-	// echo $v," ".($t->isUserDefined()?'true':'false'),"\n";
-	if($t->isUserDefined())
+	if($t->isUserDefined()&&($classname = $t->getName()) != 'Log')
 	{
-		$reflection_methods = $t->getMethods();
-		foreach ($reflection_methods as  $reflection_method) 
-		{	
-			// var_dump($reflection_method);
-			var_dump($reflection_method->getParameters());
+		$modact[$classname] =get_class_methods($classname);	
+	}
+
+}
+
+function write($modact)
+{
+	$linefeed = PHP_EOL;
+	$space    = '	';
+
+	foreach ($modact as  $modname=>$acts) 
+	{
+		$content           = 'class '.$modname.$linefeed.$linefeed;      
+		$lambdaContent     = 'class '.$modname.$linefeed.$linefeed;
+
+		foreach ($acts as $actname => $act)
+		{
+			$content       .= $space.'def '.$act.$linefeed;                
+			$content       .= 
+<<<EOF
+		@heibangUrlQueryParam.parameter = {'mod'=>'$modname','act'=>'$act'}
+EOF;
+			$content       .= $linefeed;
+			$content       .= $space.'end'.$linefeed.$linefeed;
+			
+			$lambdaContent .= $space.'def '.$act.'(robot)'.$linefeed;
+			$lambdaContent .=
+<<<EOF
+		lambda{
+			|response_data|
+			#puts response_data		
 		}
-	}
+EOF;
+			$lambdaContent .= $linefeed;
+			$lambdaContent .= $space.'end'.$linefeed.$linefeed;
 
-}
 
-class Log{
-	function write($mod,$act)
-	{
-		$a = fopen('./api/'.$mod,'a+')
+		}
+		$content           .= 'end'.$linefeed;                           
+		$lambdaContent     .= 'end'.$linefeed;                           
+		Log::write($content,$modname,'./param/');
+		Log::write($lambdaContent,$modname,'./lambda/');
 	}
 }
+write($modact);
+
+
