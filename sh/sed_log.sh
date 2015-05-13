433e9262e2ee9e35cb6d21ecdf4a4764f4be8e28
y.zz <me@y.zz>
2015-05-13 15:03:32 +0800

    no message

--- sh/sed_log.sh
+++ sh/sed_log.sh
@@ -1,14 +1,22 @@
 # 获取游戏访问 check_user 接口的请求数据
-cat application.log |sed -n '/check_user/,/API_RESPONSE/p'|sed  -n  '/REQUEST]/,/)/p'
+cat application.log |sed -n '/check_user/,/API_RESPONSE/p'|sed  -n  '/REQUEST]/,/)/p'|php ../Readfromconsole.php
 # 获取游戏访问 check_order 接口的请求数据
-cat application.log |sed -n '/check_order/,/API_RESPONSE/p'|sed  -n  '/REQUEST]/,/)/p'
+cat application.log |sed -n '/check_order/,/API_RESPONSE/p'|sed  -n  '/REQUEST]/,/)/p'|php ../Readfromconsole.php
 # -e '/API_RAW_REQUEST]/,/)/d'删除多行。获取游戏访问  set_game_info 接口的请求数据
-cat application.log |sed -n '/set_game_info/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'
- 
+cat application.log |sed -n '/set_game_info/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|php ../Readfromconsole.php
+cat application.log |sed -n '/create_order/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|php ../Readfromconsole.php
+cat application.log |sed -n '/login/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|php ../Readfromconsole.php
+cat application.log |sed -n '/check_update/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|php ../Readfromconsole.php
+
+
 #不同游戏访问三个接口的统计
 cat application.log |sed -n '/check_user/,/API_RESPONSE/p'|sed  -n  '/REQUEST]/,/)/p'|sed -n '/app_id/,/channel_id/p'|sort |uniq -c
 cat application.log |sed -n '/check_order/,/API_RESPONSE/p'|sed  -n  '/REQUEST]/,/)/p'|sed -n '/app_id/,/channel_id/p'|sort |uniq -c
 cat application.log |sed -n '/set_game_info/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|grep -E 'app_id|game_server|channel_id'
  
 #查看create_order里面的product_name参数是否为空
-cat runtime/application.log |sed -n '/create_order/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|sort |uniq -c
+cat application.log |sed -n '/create_order/,/API_RESPONSE/p'|sed  -n  '/API_REQUEST_DATA]/,/)/p'|sort |uniq -c
+
+
+sudo sed  -i 's/20006/20004/' ../Readfromconsole.php
+
